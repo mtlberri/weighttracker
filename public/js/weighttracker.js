3198e0bf67963409060182a21921d1ca9579657b
joffrey <joffrey.armellini@gmail.com>
2017-04-10 19:12:01 -0400

    improvement in the time scale management

--- public/js/weighttracker.js
+++ public/js/weighttracker.js
@@ -9,6 +9,26 @@ app.config(['ChartJsProvider', function (ChartJsProvider) {
       animation: {
       	// animation duration in ms
       	duration: 1000,
+      },
+      scales: {
+      	xAxes: [{
+      		type: 'time',
+      		/*
+      		time: {
+	      		displayFormats: {
+		           'millisecond': 'MMM DD',
+		           'second': 'MMM DD',
+		           'minute': 'MMM DD',
+		           'hour': 'MMM DD',
+		           'day': 'MMM DD',
+		           'week': 'MMM DD',
+		           'month': 'MMM DD',
+		           'quarter': 'MMM DD',
+		           'year': 'MMM DD',
+	        	}
+      		}
+      		*/
+      	}]
       }
     });
   }]);
