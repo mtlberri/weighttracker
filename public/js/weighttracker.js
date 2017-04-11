bd6476079ee0215bc050c73c1d348b424da3f4a4
joffrey <joffrey.armellini@gmail.com>
2017-04-11 16:09:35 -0400

    very nice first prod version

--- public/js/weighttracker.js
+++ public/js/weighttracker.js
@@ -5,35 +5,17 @@ var app = angular.module("weighttrackerApp", ["firebase"]);
 app.constant("moment", moment);
 app.constant("Chart", Chart);
 
-// General Chart configuration
-/*
-app.config(['ChartJsProvider', function (ChartJsProvider) {
-    // Configure all charts
-    ChartJsProvider.setOptions({
-      animation: {
-      	// animation duration in ms
-      	duration: 1000,
-      },
-      scales: {
-      	xAxes: [{
-      		type: 'time',
-      	}]
-      }
-    });
-  }]);
-  */
-
 // App Controller
 app.controller("weighttrackerController", ['$scope','$timeout', '$firebaseArray','moment', 'Chart', function($scope, $timeout, $firebaseArray, moment, Chart) {
 
 	console.log("Hello there!");
 
 	// Initialize my variables
+	$scope.uid = "default_user";
 	$scope.data_weights = [0];
 	$scope.labels_time = [0];
 	var joffreyLineColor = 'rgba(66, 139, 202, 1)';
-
-
+	var chartElement = document.getElementById('WeightChart');
 
 
 	// FirebaseUI config.
@@ -42,7 +24,7 @@ app.controller("weighttrackerController", ['$scope','$timeout', '$firebaseArray'
 		signInOptions: [
 		  // Leave the lines as is for the providers you want to offer your users.
 		  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		  // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+		  firebase.auth.FacebookAuthProvider.PROVIDER_ID,
 		  // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
 		  // firebase.auth.GithubAuthProvider.PROVIDER_ID,
 		  firebase.auth.EmailAuthProvider.PROVIDER_ID
@@ -61,6 +43,18 @@ app.controller("weighttrackerController", ['$scope','$timeout', '$firebaseArray'
 	  if (user) {
 	    // User is signed in.
 	    console.log("User is signed in!");
+	    $scope.uid = user.uid;
+
+		// Firebase data binding for the USER
+		var ref = firebase.database().ref().child("users/" + $scope.uid + "/weights/");
+		// download the data into a local object
+		$scope.data = $firebaseArray(ref);
+
+		// Watch for data events and resfresh Chart upon event
+		$scope.data.$watch(function(event) {
+			refreshChart();
+		});
+
 	    var displayName = user.displayName;
 	    var email = user.email;
 	    var emailVerified = user.emailVerified;
@@ -80,6 +74,7 @@ app.controller("weighttrackerController", ['$scope','$timeout', '$firebaseArray'
 
 			//Enable Ordering Form
 			$("#weightInputField").prop("disabled", false);
+			$("#weightCheckInButton").prop("disabled", false);
 
 			// Set User Photo
 			if (photoURL != null) {
@@ -105,13 +100,25 @@ app.controller("weighttrackerController", ['$scope','$timeout', '$firebaseArray'
 	  } else {
 	    // User is signed out.
 	    console.log("User is signed out!");
+	    $scope.uid = "default_user";
+
+	    // Firebase data binding for the USER
+		var ref = firebase.database().ref().child("users/" + $scope.uid + "/weights/");
+		// download the data into a local object
+		$scope.data = $firebaseArray(ref);
+
+		// Watch for data events and resfresh Chart upon event
+		$scope.data.$watch(function(event) {
+			refreshChart();
+		});
 
 		$("#user_name_navbar").text("Please Sign in below");
 		$("#sign-out").text("");
 
     	//Disable Ordering Form
 		$("#weightInputField").prop("disabled", true);
-
+		$("#weightCheckInButton").prop("disabled", true);
+		
 		// Set default user image
 		document.getElementById('userPhoto').src = "images/default_user.png";
 
@@ -119,19 +126,12 @@ app.controller("weighttrackerController", ['$scope','$timeout', '$firebaseArray'
 		$("#firebaseui-auth-container").slideDown(1000);
 
 
+
+
 	  }
 	}, function(error) {
 	  console.log(error);
 	});
-
-
-	// Get the Chart Element
-	var chartElement = document.getElementById('WeightChart');
-
-	// Firebase data binding
-	var ref = firebase.database().ref().child("weights");
-	// download the data into a local object
-	$scope.data = $firebaseArray(ref);
 
 	// function refreshing the Weight Chart
 	function refreshChart() {
@@ -165,8 +165,8 @@ app.controller("weighttrackerController", ['$scope','$timeout', '$firebaseArray'
 		            yAxes: [{
 		                ticks: {
 		                    beginAtZero:false,
-		                    min: Math.round( Math.min.apply(Math, $scope.data_weights)*0.95 ),
-		                    max: Math.round( Math.max.apply(Math, $scope.data_weights)*1.05 )
+		                    min: Math.round( Math.min.apply(Math, $scope.data_weights)*0.995 ),
+		                    max: Math.round( Math.max.apply(Math, $scope.data_weights)*1.005 )
 		                }
 		            }]
 		        }
@@ -183,11 +183,7 @@ app.controller("weighttrackerController", ['$scope','$timeout', '$firebaseArray'
 		return weightEntry.DateAndTime;
 	}
 
-	// Watch for data events and resfresh Chart upon event
-	$scope.data.$watch(function(event) {
-		//console.log(event);
-		refreshChart();
-	});
+
 
 	//Method to add a new Weight, called by the form ng-submit
 	$scope.checkInWeight = function(){
